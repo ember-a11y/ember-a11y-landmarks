67b153a8f1c2186ef6d214287752827f82320f75
Ken Sin <ken.sin@ticketfly.com>
2017-05-22 11:57:35 -0700

    Add integration tests and validations

--- addon/components/a11y-landmark.js
+++ addon/components/a11y-landmark.js
@@ -1,64 +1,84 @@
 import Ember from 'ember';
 import layout from '../templates/components/a11y-landmark';
+import LANDMARK_NAVIGATION_ROLES, { VALID_ROLES } from '../utils/landmark-navigation-roles';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
 
-const LANDMARK_NAVIGATION_ROLE = {
-    _default: 'region',
-    header: 'banner',
-    nav: 'navigation',
-    aside: 'complementary',
-    main: 'main',
-    footer: 'contentinfo',
-    form: 'form',
-    div: 'div'
-};
+/*
+    Component accepts 'tagName' and 'landmarkRole'.
 
+    The tagName is your 'landmark' and can be one of 7 values:
+    aside, footer, form, header, main, nav, div (default)
+
+    landmark is validated when computing 'ariaRole' (since tagNames can't be computed properties).
+
+    If the landmark is not a 'div' or a 'form', you will not need to set its landmarkRole -- it will
+    just use its only assigned role. 'div' and 'form' have more than one role option so they can be set.
+
+    The role values are:
+        * aside (complementary)
+        * footer (contentinfo)
+        * form (form, search)
+        * header (banner)
+        * main (main)
+        * nav (navigation)
+        * div (application, document, region or any of the previous)
+*/
 export default Ember.Component.extend({
     layout,
 
-    /* this should be one of 7 values:
-     * aside (complementary)
-     * footer (contentinfo)
-     * form (form, search)
-     * header (banner)
-     * main (main)
-     * nav (navigation)
-     * div (application, document, region or any of the previous)
-     */
     tagName: 'div',
 
-    /*should only be set if ('div' or 'form') is being used as a tagName, otherwise we don't need it.
-     * valid values: 
-     * banner
-     * navigation
-     * aside
-     * main
-     * form
-     * search
-     * application
-     * document
-     * region (default)
-     */
-    landmarkRole: 'region',
+    landmarkRole: computed('tagName', {
+        get() {
+            const landmark = get(this, 'tagName');
 
-    /*we should set an aria-role when either a native element is not used, or the native element does not have the body element as its parent. 
-     * since nothing is going to be the direct child of the body in an Ember app, we don't have to check for that. 
-     */
-    ariaRole: Ember.computed('tagName', 'landmarkRole', function() {
-        const landmark = this.get('tagName');
-        let landmarkRole = this.get('landmarkRole');
-        if (landmark === 'form' && landmarkRole === 'search') {
-            return 'search';
-        } else if (landmark === 'div') {
-            if (landmarkRole === 'form') {
-                Ember.assert('Use a form element for forms.');
-                //or search? think about this more in the context of Ember. 
-            } else {
-                return (landmarkRole);
+            return LANDMARK_NAVIGATION_ROLES[landmark];
+        },
+        set(key, value) {
+            const landmark = get(this, 'tagName');
+
+            if (!VALID_ROLES.includes(value)) {
+                Ember.assert(`Invalid landmarkRole "${value}". Must be one of ${VALID_ROLES.join(', ')}.`);
             }
-        } else {
-            return LANDMARK_NAVIGATION_ROLE[landmark] || LANDMARK_NAVIGATION_ROLE._default;
+
+            if (!(landmark === 'div' || landmark === 'form')) {
+                Ember.Logger.warn(`Setting landmarkRole is not required when the landmark is not a div or a form`);
+                return LANDMARK_NAVIGATION_ROLES[landmark];
+            }
+
+
+            if (landmark === 'form' && !(value === 'form' || value === 'search')) {
+                Ember.assert(`Invalid landmarkRole "${value}". Should be "form" or "search" for a "form" landmark.`);
+            }
+
+            return value;
         }
     }),
 
+    /*we should set an aria-role when either a native element is not used, or the native element does not have the body element as its parent.
+     * since nothing is going to be the direct child of the body in an Ember app, we don't have to check for that.
+     */
+    ariaRole: computed('tagName', 'landmarkRole', {
+        get() {
+            const landmark = get(this, 'tagName');
+            const landmarkRole = get(this, 'landmarkRole');
+            const validLandmarks = Object.keys(LANDMARK_NAVIGATION_ROLES);
 
+            if (!validLandmarks.includes(landmark)) {
+                Ember.assert(`Assertion Failed: Invalid tagName "${landmark}". Must be one of ${validLandmarks.join(', ')}.`);
+            }
+
+            if (landmark === 'form' && landmarkRole === 'search') {
+                return 'search';
+            }
+
+            if (landmark === 'div' && landmarkRole === 'form') {
+                Ember.assert('Use a form element for forms.');
+                //or search? think about this more in the context of Ember.
+            }
+
+            return landmarkRole;
+        }
+    })
 });
--- tests/integration/components/a11y-landmark-test.js
+++ tests/integration/components/a11y-landmark-test.js
@@ -1,25 +1,113 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import LANDMARK_NAVIGATION_ROLES, { VALID_ROLES } from 'ember-a11y-landmarks/utils/landmark-navigation-roles';
+import set from 'ember-metal/set';
+import run from 'ember-runloop';
 
 moduleForComponent('a11y-landmark', 'Integration | Component | a11y landmark', {
   integration: true
 });
 
-test('it renders', function(assert) {
-
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-
-  this.render(hbs`{{a11y-landmark}}`);
-
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
+test('default tagname and role', function(assert) {
   this.render(hbs`
     {{#a11y-landmark}}
-      template block text
+      default landmark
     {{/a11y-landmark}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('div').text().trim(), 'default landmark', 'renders div by default');
+  assert.equal(this.$('div').attr('role'), 'region', 'landmark role is region by default');
+});
+
+test('search form', function(assert) {
+  this.render(hbs`
+    {{#a11y-landmark tagName='form' landmarkRole='search'}}
+      search form
+    {{/a11y-landmark}}
+  `);
+
+  assert.equal(this.$('form').text().trim(), 'search form', 'renders form');
+  assert.equal(this.$('form').attr('role'), 'search', 'landmark role is search');
+});
+
+test('sets role automatically for each landmark', function(assert) {
+  const landmarks = Object.keys(LANDMARK_NAVIGATION_ROLES);
+
+  assert.expect(landmarks.length);
+
+  landmarks.forEach((landmark) => {
+    run(() => { set(this, 'tagName', landmark); });
+
+    this.render(hbs`
+      {{#a11y-landmark tagName=tagName}}
+        automatic
+      {{/a11y-landmark}}
+    `);
+
+    let landmarkRole = LANDMARK_NAVIGATION_ROLES[landmark];
+
+    assert.equal(this.$(landmark).attr('role'), landmarkRole, `landmark role for ${landmark} is ${landmarkRole}`);
+  });
+});
+
+test('asserts form role', function(assert) {
+  const expectedErrorMessage = 'Use a form element for forms.';
+
+  assert.expectAssertion(() => {
+    this.render(hbs`
+      {{#a11y-landmark landmarkRole='form'}}
+        assert form
+      {{/a11y-landmark}}
+    `);
+  }, expectedErrorMessage);
+});
+
+test('errors when tagName value is not a landmark navigation role', function(assert) {
+  const validLandmarks = Object.keys(LANDMARK_NAVIGATION_ROLES).join(', ');
+  const expectedErrorMessage = `Assertion Failed: Invalid tagName "someInvalidTagName". Must be one of ${validLandmarks}.`;
+
+  assert.expectAssertion(() => {
+    this.render(hbs`
+      {{#a11y-landmark tagName='someInvalidTagName'}}
+        restricted
+      {{/a11y-landmark}}
+    `);
+  }, expectedErrorMessage);
+});
+
+test('errors when landmarkRole is not a valid role', function(assert) {
+  const validRoles = VALID_ROLES.join(', ');
+  const expectedErrorMessage =  `Invalid landmarkRole "someInvalidRole". Must be one of ${validRoles}.`;
+
+  assert.expectAssertion(() => {
+    this.render(hbs`
+      {{#a11y-landmark landmarkRole='someInvalidRole'}}
+        invalid role
+      {{/a11y-landmark}}
+    `);
+  }, expectedErrorMessage);
+});
+
+test('warns when landmarkRole is set for a landmark that is not a div or a form', function(assert) {
+  const expectedWarningMessage =  "Setting landmarkRole is not required when the landmark is not a div or a form";
+
+  assert.logs('warn', expectedWarningMessage, () => {
+    this.render(hbs`
+      {{#a11y-landmark tagName='nav' landmarkRole='banner'}}
+        not div or form
+      {{/a11y-landmark}}
+    `);
+  });
+});
+
+test('errors when landmarkRole is not "form" or "search" for a "form" landmark', function(assert) {
+  const expectedErrorMessage =  'Invalid landmarkRole "main". Should be "form" or "search" for a "form" landmark.';
+
+  assert.expectAssertion(() => {
+    this.render(hbs`
+      {{#a11y-landmark tagName='form' landmarkRole='main'}}
+        invalid form
+      {{/a11y-landmark}}
+    `);
+  }, expectedErrorMessage);
 });
